e4de577c73edc856d6321ac3d18b7cd89465caf5
spadarynjaALINA <alinagusarik@gmail.com>
2022-07-10 15:37:52 +0300

    feat:add funcs for fvorites

--- graphql-serivce/src/graphql.ts
+++ graphql-serivce/src/graphql.ts
@@ -232,4 +232,13 @@ export interface JWT {
     jwt: string;
 }
 
+export interface Favorite {
+    _id: string;
+    userId: string;
+    bandsIds?: string[];
+    genresIds?: string[];
+    artistsIds?: string[];
+    tracksIds?: string[];
+}
+
 type Nullable<T> = T | null;
--- graphql-serivce/src/modules/favourites/services/favourites.service.ts
+++ graphql-serivce/src/modules/favourites/services/favourites.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
-
+import axios, { AxiosInstance } from 'axios';
+import { getItem } from 'src/utils/utils';
 @Injectable()
-export class FavouritesService {}
+export class FavouritesService {
+  private readonly url: AxiosInstance;
+  private readonly baseURL: string;
+  constructor() {
+    this.baseURL = process.env.FAVOURITES_URL;
+    this.url = axios.create({ baseURL: this.baseURL });
+  }
+
+  async getFavourites(limit: number, offset: number) {
+    getItems(limit, offset, this.url);
+  }
+  async createFavourite(favourite: NewFavourite, token: string) {
+    createItem(favourite, token, this.url);
+  }
+
+  async updateFavourite(id: string, favourite: UpdateFavourite, token: string) {
+    updateItem(id, token, this.url, favourite);
+  }
+  async deleteFavourite(id: string, token: string) {
+    deleteItem(id, token, this.url);
+  }
+}
